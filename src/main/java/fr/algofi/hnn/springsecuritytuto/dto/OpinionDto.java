a3a561a192da267caee60992dc302a4f45714e5c
hnnfr <59726038+hnnfr@users.noreply.github.com>
2024-10-10 23:22:25 +0200

    A Rest API without security

---
+++ src/main/java/fr/algofi/hnn/springsecuritytuto/dto/OpinionDto.java
@@ -0,0 +1,15 @@
+package fr.algofi.hnn.springsecuritytuto.dto;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+
+@Data
+public class OpinionDto {
+    @JsonBackReference(value = "user-opinions")
+    private UserDto user;
+
+    @JsonBackReference(value = "topic-opinions")
+    private TopicDto topic;
+
+    private String details;
+}
---
+++ src/main/java/fr/algofi/hnn/springsecuritytuto/dto/TopicDto.java
@@ -0,0 +1,19 @@
+package fr.algofi.hnn.springsecuritytuto.dto;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TopicDto {
+    private String name;
+    private String scope;
+
+    @JsonBackReference(value = "user-topics")
+    private UserDto user;
+
+    @JsonManagedReference(value = "topic-opinions")
+    private List<OpinionDto> opinions;
+}
